8a8d2537be1fe3d4fdaaf2dc984e8ba3855840e3
Ninila <alexandru.marian.cotac@gmail.com>
2026-05-25 13:14:03 +0300

    Feat: Added 3 alert tests for the prompt alert type

--- alerts_test.py
+++ alerts_test.py
@@ -2,6 +2,7 @@ from playwright.sync_api import Page, expect
 
 url = "https://www.qa-practice.com/elements/alert/alert#"
 
+
 # This test is for a simple alert message where "OK" is the only option
 def test_alert_box(page: Page):
     page.goto(url)
@@ -70,3 +71,54 @@ def test_confirmation_box_ok(page: Page):
     # We check the result of our selection is displayed on the page after clicking "OK"
     expect(page.get_by_text("You selected Ok")).to_be_visible()
 
+
+# Testing the prompt alert with the cancel option
+def test_prompt_alert_cancel(page: Page):
+    page.goto(url)
+    page.get_by_role("link", name="Prompt box").click()
+
+    # Handling the alert listener
+    def handle_dialog(dialog):
+        assert dialog.type == 'prompt'
+        dialog.dismiss()
+
+    page.on("dialog", handle_dialog)
+
+    # Assert that clicking cancel will display the "You canceled the prompt" message
+    page.get_by_role("link", name="Click").click()
+    expect(page.get_by_text("You canceled the prompt")).to_be_visible()
+
+
+# Testing the prompt alert using the "OK" option, but without completing the text field
+def test_prompt_alert_ok_notext(page: Page):
+    page.goto(url)
+    page.get_by_role("link", name="Prompt box").click()
+
+    # Handling the alert listener
+    def handle_dialog(dialog):
+        assert dialog.type == 'prompt'
+        dialog.accept()
+
+    page.on("dialog", handle_dialog)
+
+    # Assert that clicking cancel will display the "You entered nothing" message
+    page.get_by_role("link", name="Click").click()
+    expect(page.get_by_text("You entered nothing")).to_be_visible()
+
+
+# Testing the prompt alert using the "OK" option and completing the text field
+def test_prompt_alert_ok_withtext(page: Page):
+    page.goto(url)
+    page.get_by_role("link", name="Prompt box").click()
+    prompt_input = "Joe Dane"
+
+    # Handling the alert listener
+    def handle_dialog(dialog):
+        assert dialog.type == 'prompt'
+        dialog.accept(prompt_input)
+
+    page.on("dialog", handle_dialog)
+
+    # Assert that clicking cancel will display the "You entered nothing" message
+    page.get_by_role("link", name="Click").click()
+    expect(page.get_by_text(f"You entered {prompt_input}")).to_be_visible()
